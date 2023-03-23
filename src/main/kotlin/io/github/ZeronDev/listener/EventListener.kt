703ad5d5f82e1ca9a747170540bc22ec330da10f
ZeronDev <zeron_0304@naver.com>
2023-03-24 06:26:52 +0900

    2.0.7

--- src/main/kotlin/io/github/ZeronDev/listener/EventListener.kt
+++ src/main/kotlin/io/github/ZeronDev/listener/EventListener.kt
@@ -8,22 +8,21 @@ import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 
 object EventListener {
-    object EventListener {
-        inline fun <reified T : Event>listen(
-            eventPriority: EventPriority = EventPriority.NORMAL,
-            noinline func: (ListenerManager<T>).() -> Unit
-        ) : Listener {
-            val listener = object : Listener {}
-            plugin.server.pluginManager.registerEvent(T::class.java, listener, eventPriority, { _, event ->
-                if (event is T) {
-                    val listenerManager = ListenerManager(event).apply(func)
-                    if (listenerManager.isRequired) {
-                        func(listenerManager)
-                    }
-                    func(ListenerManager(event).apply(func))
+    inline fun <reified T : Event> listen(
+        eventPriority: EventPriority = EventPriority.NORMAL,
+        noinline func: (ListenerManager<T>).() -> Unit
+    ): Listener {
+        val listener = object : Listener {}
+        plugin.server.pluginManager.registerEvent(T::class.java, listener, eventPriority, { _, event ->
+            if (event is T) {
+                val listenerManager = ListenerManager(event).apply(func)
+                if (listenerManager.isRequired) {
+                    func(listenerManager)
                 }
-            }, plugin)
-            return listener
-        }
+                func(ListenerManager(event).apply(func))
+            }
+        }, plugin)
+        return listener
     }
+
 }
