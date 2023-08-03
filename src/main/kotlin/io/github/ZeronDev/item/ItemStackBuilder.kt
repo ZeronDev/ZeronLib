4b848543d804bc741d1ca8ebd705f319827fa1d1
ZeronDev <zeron_0304@naver.com>
2023-08-04 07:32:39 +0900

    2.7.1 - 냐녀의 요청

--- src/main/kotlin/io/github/ZeronDev/item/ItemStackBuilder.kt
+++ src/main/kotlin/io/github/ZeronDev/item/ItemStackBuilder.kt
@@ -1,66 +1,66 @@
 package io.github.ZeronDev.item
-
-import io.github.ZeronDev.LibraryPlugin
-import io.github.ZeronDev.config.ConfigHandler.Companion.serializeToByteArray
-import io.github.ZeronDev.item.ItemListener.register
-import net.kyori.adventure.text.Component.text
-import org.bukkit.Material
-import org.bukkit.NamespacedKey
-import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.inventory.ItemStack
-import org.bukkit.persistence.PersistentDataType
-import java.io.Serializable
-
-class ItemStackBuilder(val material: Material) : Serializable {
-    init {
-        register()
-    }
-
-    @Transient val item: ItemStack = ItemStack(material)
-    lateinit var interactFunc: (PlayerInteractEvent) -> Unit
-
-    fun displayName(name: String) : ItemStackBuilder {
-        item.itemMeta = item.itemMeta.apply {
-            this.displayName(text(name))
-        }
-        return this
-    }
-    fun lore(list: MutableList<String>) : ItemStackBuilder {
-        item.itemMeta = item.itemMeta.apply {
-            this.lore(list.map { text(it) })
-        }
-        return this
-    }
-    fun amount(amount: Int) : ItemStackBuilder {
-        item.amount = amount
-        return this
-    }
-    fun modelData(data: Int) : ItemStackBuilder {
-        item.itemMeta = item.itemMeta.apply {
-            this.setCustomModelData(data)
-        }
-        return this
-    }
-    fun <T, Z : Any>data(key: NamespacedKey, type: PersistentDataType<T, Z>, value: Z) : ItemStackBuilder {
-        item.itemMeta = item.itemMeta.apply {
-            this.persistentDataContainer.set(key, type, value)
-        }
-        return this
-    }
-
-    fun build() : ItemStack = item
-
-    fun cannotDrag() : ItemStackBuilder {
-        item.itemMeta = item.itemMeta.apply {
-            this.persistentDataContainer.set(NamespacedKey(LibraryPlugin.plugin, "cannotDrag"), PersistentDataType.STRING, "TRUE")
-        }
-        return this
-    }
-    fun onInteract(func: (PlayerInteractEvent) -> Unit) : ItemStackBuilder {
-        interactFunc = func
-        item.itemMeta = item.itemMeta?.apply {
-            this.persistentDataContainer.set(NamespacedKey(LibraryPlugin.plugin, "InteractFun"), PersistentDataType.BYTE_ARRAY, serializeToByteArray()!!)
-        }
-        return this
-    }
-}
+//
+//import io.github.ZeronDev.LibraryPlugin
+//import io.github.ZeronDev.config.ConfigHandler.Companion.serializeToByteArray
+//import io.github.ZeronDev.item.ItemListener.register
+//import net.kyori.adventure.text.Component.text
+//import org.bukkit.Material
+//import org.bukkit.NamespacedKey
+//import org.bukkit.event.player.PlayerInteractEvent
+//import org.bukkit.inventory.ItemStack
+//import org.bukkit.persistence.PersistentDataType
+//import java.io.Serializable
+//
+//class ItemStackBuilder(val material: Material) : Serializable {
+//    init {
+//        register()
+//    }
+//
+//    @Transient val item: ItemStack = ItemStack(material)
+//    lateinit var interactFunc: (PlayerInteractEvent) -> Unit
+//
+//    fun displayName(name: String) : ItemStackBuilder {
+//        item.itemMeta = item.itemMeta.apply {
+//            this.displayName(text(name))
+//        }
+//        return this
+//    }
+//    fun lore(list: MutableList<String>) : ItemStackBuilder {
+//        item.itemMeta = item.itemMeta.apply {
+//            this.lore(list.map { text(it) })
+//        }
+//        return this
+//    }
+//    fun amount(amount: Int) : ItemStackBuilder {
+//        item.amount = amount
+//        return this
+//    }
+//    fun modelData(data: Int) : ItemStackBuilder {
+//        item.itemMeta = item.itemMeta.apply {
+//            this.setCustomModelData(data)
+//        }
+//        return this
+//    }
+//    fun <T, Z : Any>data(key: NamespacedKey, type: PersistentDataType<T, Z>, value: Z) : ItemStackBuilder {
+//        item.itemMeta = item.itemMeta.apply {
+//            this.persistentDataContainer.set(key, type, value)
+//        }
+//        return this
+//    }
+//
+//    fun build() : ItemStack = item
+//
+//    fun cannotDrag() : ItemStackBuilder {
+//        item.itemMeta = item.itemMeta.apply {
+//            this.persistentDataContainer.set(NamespacedKey(LibraryPlugin.plugin, "cannotDrag"), PersistentDataType.STRING, "TRUE")
+//        }
+//        return this
+//    }
+//    fun onInteract(func: (PlayerInteractEvent) -> Unit) : ItemStackBuilder {
+//        interactFunc = func
+//        item.itemMeta = item.itemMeta?.apply {
+//            this.persistentDataContainer.set(NamespacedKey(LibraryPlugin.plugin, "InteractFun"), PersistentDataType.BYTE_ARRAY, serializeToByteArray()!!)
+//        }
+//        return this
+//    }
+//}
