f1f191eacd314b5be709797852be9ed3ca3f83a5
ZeronDev <zeron_0304@naver.com>
2023-09-09 07:34:30 +0900

    2.7.2 - item 클래스 재추가

--- src/main/kotlin/io/github/ZeronDev/item/ItemListener.kt
+++ src/main/kotlin/io/github/ZeronDev/item/ItemListener.kt
@@ -1,36 +1,36 @@
 package io.github.ZeronDev.item
-//
-//import io.github.ZeronDev.LibraryPlugin.plugin
-//import io.github.ZeronDev.config.ConfigHandler.Companion.deserializeToObject
-//import org.bukkit.NamespacedKey
-//import org.bukkit.event.EventHandler
-//import org.bukkit.event.Listener
-//import org.bukkit.event.inventory.InventoryClickEvent
-//import org.bukkit.event.inventory.InventoryDragEvent
-//import org.bukkit.event.player.PlayerInteractEvent
-//import org.bukkit.persistence.PersistentDataType
-//
-//object ItemListener : Listener {
-//    fun register() {
-//        plugin.server.pluginManager.registerEvents(this, plugin)
-//    }
-//    @EventHandler
-//    fun onInteract(e: PlayerInteractEvent) {
-//        if (e.item?.itemMeta?.persistentDataContainer?.has(NamespacedKey(plugin, "InteractFun")) == true) {
-//            e.item?.itemMeta?.persistentDataContainer?.get(NamespacedKey(plugin, "InteractFun")
-//                , PersistentDataType.BYTE_ARRAY)?.deserializeToObject(ItemStackBuilder::class)?.interactFunc?.invoke(e)
-//        }
-//    }
-//    @EventHandler
-//    fun onClick(e: InventoryClickEvent) {
-//        if (e.currentItem?.itemMeta?.persistentDataContainer?.has(NamespacedKey(plugin, "cannotDrag")) == true) {
-//            e.isCancelled
-//        }
-//    }
-//    @EventHandler
-//    fun onDrag(e: InventoryDragEvent) {
-//        if (e.cursor?.itemMeta?.persistentDataContainer?.has(NamespacedKey(plugin, "cannotDrag")) == true) {
-//            e.isCancelled = true
-//        }
-//    }
-//}
+
+import io.github.ZeronDev.LibraryPlugin.plugin
+import io.github.ZeronDev.config.ConfigHandler.Companion.deserializeToObject
+import org.bukkit.NamespacedKey
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryDragEvent
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.persistence.PersistentDataType
+
+object ItemListener : Listener {
+    fun register() {
+        plugin.server.pluginManager.registerEvents(this, plugin)
+    }
+    @EventHandler
+    fun onInteract(e: PlayerInteractEvent) {
+        if (e.item?.itemMeta?.persistentDataContainer?.has(NamespacedKey(plugin, "InteractFun")) == true) {
+            e.item?.itemMeta?.persistentDataContainer?.get(NamespacedKey(plugin, "InteractFun")
+                , PersistentDataType.BYTE_ARRAY)?.deserializeToObject(ItemStackBuilder::class)?.interactFunc?.invoke(e)
+        }
+    }
+    @EventHandler
+    fun onClick(e: InventoryClickEvent) {
+        if (e.currentItem?.itemMeta?.persistentDataContainer?.has(NamespacedKey(plugin, "cannotDrag")) == true) {
+            e.isCancelled
+        }
+    }
+    @EventHandler
+    fun onDrag(e: InventoryDragEvent) {
+        if (e.cursor?.itemMeta?.persistentDataContainer?.has(NamespacedKey(plugin, "cannotDrag")) == true) {
+            e.isCancelled = true
+        }
+    }
+}
